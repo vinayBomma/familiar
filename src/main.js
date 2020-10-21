91359839669e8d85206da60818f865e37b93139f
vinayBomma <vinaysb2000@gmail.com>
2020-10-21 18:27:23 +0530

    Added Leaflet. Displays map and marker.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
+
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="height: 750px;">
+    <div class="info">
+      <span>Center: {{ center }}</span>
+      <span>Zoom: {{ zoom }}</span>
+      <span>Bounds: {{ bounds }}</span>
+    </div>
+    <l-map
+      style="height: 80%; width: 80%"
+      :zoom="zoom"
+      :center="center"
+      @update:zoom="zoomUpdated"
+      @update:center="centerUpdated"
+      @update:bounds="boundsUpdated"
+    >
+      <l-tile-layer :url="url"></l-tile-layer>
+      <l-marker :lat-lng="markerLatLng" :icon="defIcon">
+        <l-popup>
+          <h3>Amyra is in Mumbai</h3>
+          <p>She is in a Park</p>
+        </l-popup>
+      </l-marker>
+    </l-map>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import {LMap, LTileLayer, LMarker, LPopup} from 'vue2-leaflet';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld,
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup
+  },
+   data () {
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      zoom: 3,
+      center: [47.413220, -1.219482],
+      bounds: null,
+      markerLatLng: [47.413220, -1.219482],
+      defIcon: L.icon({
+        iconUrl: 'https://www.flaticon.com/svg/static/icons/svg/3048/3048122.svg',
+        iconSize: [38, 95],
+      })
+    };
+  },
+  methods: {
+    zoomUpdated (zoom) {
+      this.zoom = zoom;
+    },
+    centerUpdated (center) {
+      this.center = center;
+    },
+    boundsUpdated (bounds) {
+      this.bounds = bounds;
+    }
   }
 }
 </script>
